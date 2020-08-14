26c3e14807859cf359d9f535d4efe89932e6f282
Rohit Sharma <rohit.sharma02@sjsu.edu>
2020-08-14 05:43:13 -0400

    LinkedList Basics

---
+++ src/com/company/linkedlist/LinkedListTraversal.java
@@ -0,0 +1,34 @@
+package com.company.linkedlist;
+
+public class LinkedListTraversal {
+    private Node head;
+
+    static class Node{
+        int data;
+        Node next;
+        public Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void printList() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data + " ");
+            n = n.next;
+        }
+    }
+    // driver class
+    public static void main (String[] args){
+            LinkedListTraversal list = new LinkedListTraversal();
+            list.head = new Node(1);
+            Node second = new Node(2);
+            Node third = new Node(3);
+
+            list.head.next = second;
+            second.next = third;
+
+            list.printList();
+    }
+}
--- src/com/company/linkedlist/SimpleLinkedListThreeNodes.java
+++ src/com/company/linkedlist/SimpleLinkedListThreeNodes.java
@@ -1,8 +1,21 @@
 package com.company.linkedlist;
 
 public class SimpleLinkedListThreeNodes {
-
-    class Node{
-        
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next =null;
+        }
     }
+    // driver class
+    public static void main(String[] args){
+        SimpleLinkedListThreeNodes list = new SimpleLinkedListThreeNodes();
+        list.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+    }
+
 }
